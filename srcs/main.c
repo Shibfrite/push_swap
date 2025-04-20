95e4a8c404272802882b75fad0b3074219e08725
Kurek Maximilien <kurekmaximilien@gmail.com>
2025-04-20 17:05:02 +0200

    mhh

--- srcs/main.c
+++ srcs/main.c
@@ -1,52 +1,73 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                         ::::::::           */
-/*   main.c                                              :+:    :+:           */
-/*                                                      +:+                   */
-/*   By: makurek <marvin@42.fr>                        +#+                    */
-/*                                                    +#+                     */
-/*   Created: 2025/03/28 16:39:55 by makurek        #+#    #+#                */
-/*   Updated: 2025/04/02 17:42:32 by makurek        ########   odam.nl        */
-/*                                                                            */
+/*																			*/
+/*														 ::::::::		   */
+/*   main.c											  :+:	:+:		   */
+/*													  +:+				   */
+/*   By: makurek <marvin@42.fr>						+#+					*/
+/*													+#+					 */
+/*   Created: 2025/03/28 16:39:55 by makurek		#+#	#+#				*/
+/*   Updated: 2025/04/02 17:42:32 by makurek		########   odam.nl		*/
+/*																			*/
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int find_lowest_value_above_threshold(t_dnode *head, int threshold) {
-    if (!head) return INT_MAX;
-    int min_value = INT_MAX;
-    t_dnode *current = head;
+int	find_lowest_value_above_threshold(t_dnode *head, int threshold)
+{
+	int		min_value;
+	t_dnode	*current;
+	int		current_value;
 
-    while (current) {
-        int current_value = *(int *)(current->data);
-        if (current_value > threshold && current_value < min_value) {
-            min_value = current_value; // Update min_value if a smaller valid value is found
-        }
-        current = current->next;
-    }
-    return min_value; // Return INT_MAX if no valid value was found
+	if (!head)
+		return (INT_MAX);
+	min_value = INT_MAX;
+	current = head;
+	while (current)
+	{
+		current_value = *(int *)(current->data);
+		if (current_value > threshold && current_value < min_value)
+			min_value = current_value;
+		current = current->next;
+	}
+	return (min_value);
 }
 
-void replace_values_in_dlist(t_dnode *head) {
-    if (!head) return; // Empty list
+void	replace_values_in_dlist(t_dnode *head)
+{
+	int	threshold;
+	int	replacement_value;
 
-    int threshold = INT_MIN; // Start with the smallest possible integer
-    int replacement_value = 0; // Replacement starts at 0
+	if (!head)
+		return ;
+	threshold = INT_MIN;
+	replacement_value = 0;
+	while (1)
+		if (!find_and_replace_next_value(head, &threshold, &replacement_value))
+			break ;
+}
 
-    while (1) {
-        int lowest_value = find_lowest_value_above_threshold(head, threshold);
-        if (lowest_value == INT_MAX) break;
-        t_dnode *current = head;
-        while (current) {
-            if (*(int *)(current->data) == lowest_value) {
-                *(int *)(current->data) = replacement_value;
-                break;
-            }
-            current = current->next;
-        }
-        threshold = lowest_value;
-        replacement_value++;
-    }
+int	find_and_replace_next_value(t_dnode *head,
+			int *threshold, int *replacement_value)
+{
+	int		lowest_value;
+	t_dnode	*current;
+
+	lowest_value = find_lowest_value_above_threshold(head, *threshold);
+	if (lowest_value == INT_MAX)
+		return (0);
+	current = head;
+	while (current)
+	{
+		if (*(int *)(current->data) == lowest_value)
+		{
+			*(int *)(current->data) = *replacement_value;
+			break ;
+		}
+		current = current->next;
+	}
+	*threshold = lowest_value;
+	(*replacement_value)++;
+	return (1);
 }
 
 int	main(int argc, char *argv[])
@@ -66,4 +87,4 @@ int	main(int argc, char *argv[])
 		ft_dlstclear(&stacks.b, free);
 	return (SUCCESS);
 }
-//print_list(stacks.a); //DEBUG
+//print_list(stacks.a);
--- srcs/sort_radix.c
+++ srcs/sort_radix.c
@@ -23,16 +23,16 @@ static void	radix_sort_stack_b(t_stacks *stacks,
 
 	size = count_stack_size(stacks->b);
 	processed = 0;
-	while (processed < size && stacks->b && is_sorted(stacks->a) == ERROR)
+	while (processed < size && is_sorted(stacks->a) == ERROR)
 	{
-		if (((*(int *)(stacks->b->data) >> j) & 1) == 0)
+		if (((*(int *)stacks->b->data >> j) & 1) == 0)
 			add_operation(operations_list, stacks, "rb", 1);
 		else
 			add_operation(operations_list, stacks, "pa", 1);
 		processed++;
 	}
-	while (stacks->b)
-		add_operation(operations_list, stacks, "pa", 1);
+	//while (stacks->b)
+	//	add_operation(operations_list, stacks, "pa", 1);
 }
 
 static int	calculate_bit_size(int num)
@@ -40,52 +40,28 @@ static int	calculate_bit_size(int num)
 	int	bit_size;
 
 	bit_size = 0;
-	while (num > 0)
+	while (num > 1)
 	{
-		num >>= 1;
+		num /= 2;
 		bit_size++;
 	}
 	return (bit_size);
 }
 
-static int	find_max_in_dlist(t_dnode *head)
-{
-	int		max_value;
-	int		current_value;
-	t_dnode	*current;
-
-	if (!head)
-		return (INT_MIN);
-	max_value = *(int *)(head->data);
-	current = head->next;
-	while (current)
-	{
-		current_value = *(int *)(current->data);
-		if (current_value > max_value)
-			max_value = current_value;
-		current = current->next;
-	}
-	return (max_value);
-}
-
 void	radix_sort(t_stacks *stacks, t_dnode **operations_list, int total)
 {
-	int	max_value;
 	int	bit_size;
 	int	j;
 	int	size;
 
-	max_value = find_max_in_dlist(stacks->a);
-	if (max_value == INT_MIN)
-		return ;
-	bit_size = calculate_bit_size(max_value);
+	bit_size = calculate_bit_size(total);
 	j = -1;
-	while (++j < bit_size)
+	while (++j <= bit_size)
 	{
 		size = total;
-		while (size-- && stacks->a && is_sorted(stacks->a) == ERROR)
+		while (size-- && is_sorted(stacks->a) == ERROR)
 		{
-			if (((*(int *)(stacks->a->data) >> j) & 1) == 0)
+			if (((*(int *)stacks->a->data >> j) & 1) == 0)
 				add_operation(operations_list, stacks, "pb", 1);
 			else
 				add_operation(operations_list, stacks, "ra", 1);
